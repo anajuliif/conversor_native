dad1bc5342a8caa2583e3ca4f4e246bfc4fe82c4
anajuliif <anafeereira125@gmail.com>
2024-04-10 15:38:44 -0300

    commit

--- App.js
+++ App.js
@@ -1,11 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  TextInput,
+} from "react-native";
 
 export default function App() {
+  const [cel, setCelsius] = useState("");
+  const [fah, setFarenheit] = useState("");
+
+  function calcTemperatura() {
+    fah = (parseFloat(cel) * 9) / 5 + 32;
+    window.alert("A temperatura em fahrenheit é" + fah);
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.titulo}> Conversor de temperaturas </Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Insira a temperatura em celsius"
+      />
+      <TouchableOpacity
+        style={styles.botao}
+        onPress={calcTemperatura}
+        onChangeText={(cel) => setCelsius(cel)}
+      >
+        <Text>Calcular</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -13,8 +37,20 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#e6e6fa",
+    alignItems: "center",
+  },
+  botao: {
+    borderRadius: 10,
+    backgroundColor: "#ba55d3",
+  },
+  input: {
+    borderRadius: 10,
+    backgroundColor: "white",
+    height: 20,
+    width: 220,
+  },
+  titulo: {
+    fontWeight: "bold",
   },
 });
